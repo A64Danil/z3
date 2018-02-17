054e8feed30d297c04e9016f791a705c8bd45011
Danil <a64danil@mail.ru>
2018-02-17 21:33:03 +0300

    тестируем первое задание

--- src/my.js
+++ src/my.js
@@ -1,4 +1,27 @@
-/**
- * Created by Danil on 15.02.2018.
- */
 console.log("NODE js working. Русские символы");
+
+/*
+ Задача 1:
+ Напишите аналог встроенного метода forEach для работы с массивами
+ */
+
+var arr = [3,2,5];
+
+
+function forEach(array, fn) {
+
+    for (let i=0; i < array.length ; i++) {
+        fn(array[i], i, array);
+
+    }
+
+}
+
+
+function conLog(val, index, array) {
+    console.log(val + 2, "index: " + index, array);
+}
+
+
+forEach(arr, conLog);
+
